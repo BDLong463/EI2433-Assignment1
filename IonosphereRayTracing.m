526985e6603de3dd9c2bb88ab54d176aea97498f
BDLong463 <108671411+BDLong463@users.noreply.github.com>
2023-11-17 15:29:31 +0000

    Add files via upload

--- IonosphereRayTracing.m
+++ IonosphereRayTracing.m
@@ -1,111 +1,111 @@
-function [PF Z0 neZ zUpperLim xMaximum f_reflex XLinear ZLinear ZRefLinear NLX NLZ breakpoint] = IonosphereRayTracing(ParameterArray)
+function [PF z Z1 neZ zUpperLim xMaximum f_reflex XLinear ZLinear ZRefLinear NLX NLZ breakpoint alpha_nVector] = IonosphereRayTracing(ParameterArray);
     
+    %Physical constants
     K_b=1.38065e-23;%Botlzmann contstant
     m0=1.672622e-27; %Proton mass
     g0=9.8067;% g acceleration at sea level
-    Ro=1.225;%atmospheric density at sea level
     q=1.6e-19;%Electron charge
-    E0=8.854e-12;
-    m_e=9.10938e-31;
-    zUpperLim=6e5 %Maximum z computed in m
-    %Get parameters from param().m
-    % 31/01/1998
-    alpha=2*pi/6; %Ray angle in radians (over the vertical)
-    a_i=9.3e-23;% Ionization coefficient
-    a_r=3e-14;% Recombination coefficient
-    a_a=9.3e-23;% Absorttion coefficient
-    T=500 %Temperature in kelvin
-    f=5e6 %Frequency in Hz
-    deltaZ=100 %deltaZ in meters
-    n0=2.7e25; % Neutral species density at sea level
-    I0=1.5e14;% Radiation intensity at the upper layer of the atmosphere
+    E0=8.854e-12;%Electric permittivity in vacuum
+    m_e=9.10938e-31;%Electron mass
     m=16*2*m0; %O2 mass
-    MaxComputedAngle= 89 %Maximum computed angle for step 4 in degrees
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %Get parameters from param().m
+    % Some fixed values for debug
+    %alpha=2*pi/6; %Ray angle in radians (over the vertical)
+    %a_i=9.3e-23;% Ionization coefficient
+    %a_r=3e-14;% Recombination coefficient
+    %a_a=9.3e-23;% Absorttion coefficient
+    %T=500 %Temperature in kelvin
+    %f=5e6 %Frequency in Hz
+    %n0=2.7e25; % Neutral species density at sea level
+    %I0=1.5e14;% Radiation intensity at the upper layer of the atmosphere
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    deltaZ=100; %deltaZ in meters
+    zUpperLim=6e5; %Maximum z computed in m
+    MaxComputedAngle= 89; %Maximum computed angle for step 4 in degrees
     
     %ParameterArrayValues
-    a_a=ParameterArray(1)
-    a_i=ParameterArray(2)
-    a_r=ParameterArray(3)
-    I0=ParameterArray(4)
-    n0=ParameterArray(5)
-    T=ParameterArray(6)
-    f=ParameterArray(7)
-    alpha=ParameterArray(8)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    a_a=ParameterArray(1);% Absorttion coefficient
+    a_i=ParameterArray(2);% Ionization coefficient
+    a_r=ParameterArray(3);% Recombination coefficient
+    I0=ParameterArray(4);% Radiation intensity at the upper layer of the atmosphere
+    n0=ParameterArray(5); % Neutral species density at sea level
+    T=ParameterArray(6);%Temperature in kelvin
+    f=ParameterArray(7);%Frequency in Hz
+    alpha=ParameterArray(8);%Ray angle in degrees (over the vertical)
     H=K_b*T/(m*g0); %Atmospheric scale height
-    z=0:deltaZ:zUpperLim; %Z testing vector
-    Z0=z
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    z=0:1000:zUpperLim; %Z testing vector
     ne=@(z_)((a_i/a_r)*n0.*exp(-(z_)/H)*I0.*exp(-H*a_a*n0*exp(-(z_)/H))).^0.5; %Electron density at chapman layer mathematical function
-    neZ=ne(z)
+    neZ=ne(z); %Electron density for z vector
     f_p=@(n_)((n_.*((q^2)/(E0*m_e))).^0.5)/(2*pi); %plasma frequency for a given electron density
-    f_reflex=f*cos(alpha) %Theoretical frequency of reflexion
-    PF=f_p(ne(z));
-    [dim, steps]=size(z); %CLEAN
+    f_reflex=f*cos(alpha); %Theoretical frequency of reflexion
+    PF=f_p(ne(z));%Plasma frequency for each Z
     Z1=0; %Resolution Value change
     
-    %Try to get electron density critical values by math instead of bruteforce
+    %Getting the relexion height and critical height Z1
     for k=0:deltaZ:zUpperLim
         fpi=f_p(ne(k));
-        if fpi>(f/10) && Z1==0
-            Z1=k-deltaZ-1
+        if fpi>(f_reflex/10) && Z1==0
+            Z1=k-deltaZ;
         end
         if fpi>f_reflex
-            zreflex=k-deltaZ-1
+            zreflex=k-deltaZ;
             break
         end
     end
     
-
-    
-    Zscaled=[0:floor(Z1/5):Z1,Z1:deltaZ:zreflex]; %Z vector with only 5 samples until Z1 point (linear part), then samples at deltaZ resolution defined previously
+    Zscaled=[0:floor(Z1/10):Z1,Z1:deltaZ:zreflex]; %Z vector with only 10 samples until Z1 point (linear part), then samples at deltaZ resolution defined previously
     [xz,nzreflexion]=size(Zscaled); %Get the Zscaled vector lenght "nzreflexion" used for iteration later
-    Xscaled=zeros(1,(2*nzreflexion)); %Define a X vector with twice the size of ZScaled
-    xMaximum=2*Zscaled(nzreflexion)*tan(alpha); %Maximum travelled distance (linearly), used to flip the linear ray path
-    for k=1:1:nzreflexion
-        %Compute the x value for each Z sampled (as x is mirrored is x(k) and xMaximun-x(k) are computed at the same time)
-        Xlinear=Zscaled(k)*tan(alpha);
-        Xscaled(k)=Xlinear;
-        Xscaled((2*nzreflexion)-k+1)=xMaximum-Xlinear;
-    end
-    ZCombined=[Zscaled,flip(Zscaled)]; %
-    XLinear=Xscaled;
-    ZLinear=ZCombined;
+    xMaximum=2*Zscaled(nzreflexion)*tan(alpha); %Maximum travelled distance (linearly), used to flip the linear ray path    
+    
+    %Step 3 define the simplified path with origin, reflexion and destination point
+    XLinear=[0,zreflex*tan(alpha),2*zreflex*tan(alpha)];
+    ZLinear=[0,zreflex,0];
     ZRefLinear=zreflex;
     
     
-    Extra=(Zscaled(nzreflexion)+0.1*deltaZ):(0.1*deltaZ):(Zscaled(nzreflexion)+deltaZ)
-    ZscaledExtra=[Zscaled,Extra] %Add a deltaZ lenght with 10 times the resolution to approach better to the critical angle
-    [aa,bb]=size(ZscaledExtra)
+    %Add a deltaZ lenght with 10 times the resolution to approach better to the critical angle
+    ZscaledExtra=[Zscaled,(Zscaled(nzreflexion)+0.1*deltaZ):(0.1*deltaZ):(Zscaled(nzreflexion)+deltaZ)]; 
+    [aa,bb]=size(ZscaledExtra);
     x1=zeros(size(ZscaledExtra)); %X coordinate calculate with vatiable refracting index
     Dx1=zeros(size(ZscaledExtra)); %Delta x1 used to flip the x1 vector
     alpha_n=alpha; %initial value for iterative aplha
-    alpha_nVector=zeros(1,nzreflexion); %Vector to save angle value (in degrees) for each iteration step
+    alpha_nVector=zeros(1,nzreflexion); %Vector to save angle value (in degrees) for each iteration step (debug)
     nr=@(z) (1-((f_p(ne(z))^2))/(f^2))^0.5; %Refractive index function
-    breakpoint=0 %index (iteration step) when alpha_n reaches its criticall value "MaxComputedAngle"
+    breakpoint=0; %index (iteration step) when alpha_n reaches its criticall value "MaxComputedAngle"
     
+    %Loop for each index of ZscaledExtra (1:1:bb)
     for k=1:1:bb
         if k==1
-            alpha_n=alpha
+            %For the first index as we don't have a previous value 
+            alpha_n=alpha;
             x1(k)=ZscaledExtra(k)*tan(alpha_n);
             Dx1(k)=x1(k);
         else
-            nr(ZscaledExtra(k));
-            (nr(ZscaledExtra(k-1)));
+            %Compute the new incidence angle, if is greater than 90 stop the loop and mark the previous index as the last for the final vector
             alpha_n=asin((nr(ZscaledExtra(k-1))*sin(alpha_n))/(nr(ZscaledExtra(k))));
             if alpha_n>(MaxComputedAngle*pi/180)
-                breakpoint=k-1
+                breakpoint=k-1;
                 break
             end
-            x1(k)=x1(k-1)+(ZscaledExtra(k)-ZscaledExtra(k-1))*tan(alpha_n);
-            Dx1(k)=x1(k)-x1(k-1);
-            
+            %The difference between the current X and the one previously computed is saved to flip the vector later
+            Dx1(k)=(ZscaledExtra(k)-ZscaledExtra(k-1))*tan(alpha_n);
+            %New position is computed as the difference between the current and previous Z times the tangent of alpha for its corresponding difraction index
+            x1(k)=x1(k-1)+Dx1(k);            
         end
-        alpha_nVector(k)=180*alpha_n/pi; 
+        alpha_nVector(k)=180*alpha_n/pi; %Vector with all the diffraction angles computed for each step
     end
+    %Trim the final vectors to the breakpoint and then flip Z and X
     Dx11=Dx1(1:breakpoint);
     flipDX=flip(Dx11);
     x11=x1(1:breakpoint);
     x2=zeros(1,breakpoint);
-    %alpha_nVector=(alpha_nVector(1:breakpoint));
+    alpha_nVector=(alpha_nVector(1:breakpoint));
     for k=1:1:breakpoint
         if k==1
             x2(k)=x11(breakpoint)+flipDX(k);
@@ -116,8 +116,7 @@ function [PF Z0 neZ zUpperLim xMaximum f_reflex XLinear ZLinear ZRefLinear NLX N
     
     XCombinedNL=[x11,x2]; %X combined non linear
     ZCombinedNL=[ZscaledExtra(1:breakpoint),flip(ZscaledExtra(1:breakpoint))];%Z combined non linear
-    NLX=XCombinedNL
-    NLZ=ZCombinedNL
-
+    NLX=XCombinedNL;
+    NLZ=ZCombinedNL;    
    
 end
--- IonosphericRadioWaveReflection.m
+++ IonosphericRadioWaveReflection.m
@@ -1,63 +1,113 @@
 %[PF Z0 neZ zUpperLim xMaximum f_reflex XLinear ZLinear ZRefLinear NLX NLZ breakpoint]=
-[aa ai ar I0 n0 T f alpha]=get_parameters(980131)
-[PF Z0 neZ zUpperLim xMaximum f_reflex XLinear ZLinear ZRefLinear NLX NLZ breakpoint] = IonosphereRayTracing([aa ai ar I0 n0 T f ((alpha*pi)/180)])
+[aa ai ar I0 n0 T f alpha]=get_parameters(980131);
+[PF Z0 Z1 neZ zUpperLim xMaximum f_reflex XLinear ZLinear ZRefLinear NLX NLZ breakpoint alpha_nVector] = IonosphereRayTracing([aa ai ar I0 n0 T f ((alpha*pi)/180)]);
+
+%Refraction angle vs altitude FIG8
+fig8=figure(8);
+plot(alpha_nVector,NLZ(1:breakpoint)/1e3,"-*")
+yline(Z1/1e3,"-.",["z(f_r_e_f_l_x/10): " + num2str(Z1/1e3,4)+ "km"])
+xlabel('alpha [deg]')
+ylabel('z [km]')
+title("Refraction angle vs altitude")
 
 fig1=figure(1);
-
 semilogx(neZ,Z0/1000) %SemiLog in x plot of electron density vs altitude (z in km) FIGURE1
-
+xlabel('ne [m^-^3]')
+ylabel('z [km]')
+title("Electron density vs altitude")
 fig2=figure(2);
-semilogx(PF/1000,Z0/1000) %SemiLog in x plot of flasma frequency (kHz) vs altitude (z in km), 
+semilogx(PF/1000,Z0/1000); %SemiLog in x plot of flasma frequency (kHz) vs altitude (z in km), 
 % Vertical line of emission frequency FIGURE2
-xlim([0.1,1e8/1e3])
+xlim([0.1,2e8/1e3])
 ylim([0,zUpperLim/1e3])
-xline(f_reflex/1e3,"","Reflexion frequency") %Plot vertical line at reflexion frequency
-yline(ZRefLinear/1e3,"","Reflexion altitude") %Plot horizontal line at reflexion altitude
+xline(f_reflex/1e3,"-.",["Reflexion frequency: " num2str(f_reflex/1e3,4)+ "kHz"],LabelOrientation="horizontal") %Plot vertical line at reflexion frequency
+yline(ZRefLinear/1e3,"-.",["Reflexion altitude: " + num2str(ZRefLinear/1e3,4)+ "km"],"LabelHorizontalAlignment","left") %Plot horizontal line at reflexion altitude
+xlabel('f_p [kHz]')
+ylabel('z [km]')
+title("Plasma frequency vs altitude")
 
 fig3=figure(3);
 plot(XLinear/1e3,ZLinear/1e3)
 %Plot of the coordinates of a linear ray path (both axis in km) FIGURE3
-yline(ZRefLinear/1e3,"","Reflexion altitude") %Plot horizontal line at reflexion altitude
+yline(ZRefLinear/1e3,"-.",["Reflexion altitude: " + num2str(ZRefLinear/1e3,4)+ "km"]) %Plot horizontal line at reflexion altitude
 xlabel('x [km]')
-ylabel('z [km]]')
-xlim([0.1,xMaximum/1e3]) %End the X axis at xMaximum value
+ylabel('z [km]')
+xlim([0.1,(xMaximum/1e3)+50]) %End the X axis at xMaximum value+50km
+title("Linear aproximation ray path")
 
 fig4=figure(4);
 %the reflection shall be computed with the difference of the flipped vector Zi
 plot(NLX/1e3,NLZ/1e3)
-yline(NLZ(breakpoint)/1e3,"","Reflexion altitude") %Plot horizontal line at reflexion altitude
+yline(NLZ(breakpoint)/1e3,"-.",["Reflexion altitude: "+num2str(NLZ(breakpoint)/1e3,4)+"km"]) %Plot horizontal line at reflexion altitude
 xlabel('x [km]')
-ylabel('z [km]]')
-xlim([0,NLX(2*breakpoint)/1e3]) %End the X axis at xMaximum value
+ylabel('z [km]')
+xlim([0,(NLX(2*breakpoint)/1e3)+50]) %End the X axis at xMaximum value + 50km
+title("Variable Refracive index aproximation ray path")
 
 fig5=figure(5);
 %Comparisson Figure
 plot(XLinear/1e3,ZLinear/1e3,NLX/1e3,NLZ/1e3)
 %Plot of the coordinates of a linear ray path (both axis in km) FIGURE3
-yline(ZRefLinear/1e3,"","Reflexion altitude (linear aprox)") %Plot horizontal line at reflexion altitude (linear)
-yline(NLZ(breakpoint)/1e3,"","Reflexion altitude") %Plot horizontal line at reflexion altitude
+yline(ZRefLinear/1e3,"-.",["Reflexion altitude (linear): "+num2str(ZRefLinear/1e3,4)+ "km"],LabelVerticalAlignment="bottom") %Plot horizontal line at reflexion altitude (linear)
+yline(NLZ(breakpoint)/1e3,"-.",["Reflexion altitude: "+num2str(NLZ(breakpoint)/1e3,4)+ "km"]) %Plot horizontal line at reflexion altitude
 xlabel('x [km]')
-ylabel('z [km]]')
+ylabel('z [km]')
 xlim([0,(NLX(2*breakpoint)/1e3)+50]) %End the X axis at xMaximum value +50km
+title("Linear vs Variable Refracive index aproximation")
 
 %STEP 5
-[PF1 Z01 neZ1 zUpperLim1 xMaximum1 f_reflex1 XLinear1 ZLinear1 ZRefLinear1 NLX1 NLZ1 breakpoint1] = IonosphereRayTracing([aa ai ar I0 n0 T+100 f ((alpha*pi)/180)])
+[PF1 Z01 Z1 neZ1 zUpperLim1 xMaximum1 f_reflex1 XLinear1 ZLinear1 ZRefLinear1 NLX1 NLZ1 breakpoint1] = IonosphereRayTracing([aa ai ar I0 n0 T+100 f ((alpha*pi)/180)]);
 
 fig6=figure(6);
-%the reflection shall be computed with the difference of the flipped vector Zi
+%Plot of fig4 with original parameters and with T1=T+100K
 plot(NLX/1e3,NLZ/1e3,NLX1/1e3,NLZ1/1e3)
-yline(NLZ(breakpoint)/1e3,"","Reflexion altitude") %Plot horizontal line at reflexion altitude
-yline(NLZ1(breakpoint1)/1e3,"","Reflexion altitude +100K")
+yline(NLZ(breakpoint)/1e3,"-.",["Reflexion altitude: "+num2str(NLZ(breakpoint)/1e3,4)+"km"]) %Plot horizontal line at reflexion altitude
+yline(NLZ1(breakpoint1)/1e3,"-.",["Reflexion altitude +100K: "+ num2str(NLZ1(breakpoint1)/1e3,4)+ "km"])
 xlabel('x [km]')
-ylabel('z [km]]')
+ylabel('z [km]')
+title("Variable Refracive index aproximation: "+num2str(T,4)+"K vs "+num2str(T+100,4)+"K")
+
+fig9=figure(9);
+semilogx(PF/1000,Z0/1000,PF1/1000,Z0/1000); %SemiLog in x plot of flasma frequency (kHz) vs altitude (z in km), 
+% Vertical line of emission frequency FIGURE2
+xlim([0.1,2e8/1e3])
+ylim([0,zUpperLim/1e3])
+yline(ZRefLinear1/1e3,"-.",["Reflexion altitude +100K: " + num2str(ZRefLinear1/1e3,4)+ "km"],"LabelHorizontalAlignment","left") %Plot horizontal line at reflexion altitude
+xline(f_reflex/1e3,"-.",["Reflexion frequency: " num2str(f_reflex/1e3,4)+ "kHz"],LabelOrientation="horizontal") %Plot vertical line at reflexion frequency
+yline(ZRefLinear/1e3,"-.",["Reflexion altitude: " + num2str(ZRefLinear/1e3,4)+ "km"],"LabelHorizontalAlignment","left",LabelVerticalAlignment="bottom") %Plot horizontal line at reflexion altitude
+xlabel('f_p [kHz]')
+ylabel('z [km]')
+title("Plasma frequency vs altitude")
 
 %Step 7
-[PF2 Z02 neZ2 zUpperLim2 xMaximum2 f_reflex2 XLinear2 ZLinear2 ZRefLinear2 NLX2 NLZ2 breakpoint2] = IonosphereRayTracing([aa ai ar I0*10 n0 T+100 f ((alpha*pi)/180)])
+[PF2 Z02 Z1 neZ2 zUpperLim2 xMaximum2 f_reflex2 XLinear2 ZLinear2 ZRefLinear2 NLX2 NLZ2 breakpoint2] = IonosphereRayTracing([aa ai ar I0*10 n0 T+100 f ((alpha*pi)/180)]);
 
 fig7=figure(7);
-%the reflection shall be computed with the difference of the flipped vector Zi
+%Plot of fig4 with original parameters and with 10 times I0
 plot(NLX/1e3,NLZ/1e3,NLX2/1e3,NLZ2/1e3)
-yline(NLZ(breakpoint)/1e3,"","Reflexion altitude") %Plot horizontal line at reflexion altitude
-yline(NLZ2(breakpoint2)/1e3,"","Reflexion altitude 10 times I0")
+yline(NLZ(breakpoint)/1e3,"-.",["Reflexion altitude: "+num2str(NLZ(breakpoint)/1e3,4)+ "km"]) %Plot horizontal line at reflexion altitude
+yline(NLZ2(breakpoint2)/1e3,"-.",["Reflexion altitude 10*I_0: "+num2str(NLZ2(breakpoint2)/1e3,4)+ "km"],LabelVerticalAlignment="bottom")
 xlabel('x [km]')
-ylabel('z [km]]')
+ylabel('z [km]')
+xlim([0,(NLX(2*breakpoint)/1e3)+50]) %End the X axis at xMaximum value +50km
+title("Variable Refracive index aproximation I_0: "+num2str(I0,'%.2e')+" vs "+num2str(10*I0,'%.2e'))
+
+
+fig10=figure(10);
+semilogx(PF/1000,Z0/1000,PF2/1000,Z0/1000); %SemiLog in x plot of flasma frequency (kHz) vs altitude (z in km), 
+% Vertical line of emission frequency FIGURE2
+xlim([0.1,2e8/1e3])
+ylim([0,zUpperLim/1e3])
+yline(ZRefLinear2/1e3,"-.",["Reflexion altitude 10*I0: " + num2str(ZRefLinear2/1e3,4)+ "km"],"LabelHorizontalAlignment","left",LabelVerticalAlignment="bottom") %Plot horizontal line at reflexion altitude
+xline(f_reflex/1e3,"-.",["Reflexion frequency: " num2str(f_reflex/1e3,4)+ "kHz"],LabelOrientation="horizontal") %Plot vertical line at reflexion frequency
+yline(ZRefLinear/1e3,"-.",["Reflexion altitude: " + num2str(ZRefLinear/1e3,4)+ "km"],"LabelHorizontalAlignment","left") %Plot horizontal line at reflexion altitude
+xlabel('f_p [kHz]')
+ylabel('z [km]')
+title("Plasma frequency vs altitude")
+
+str = ['x max: Linear aprox    = ' num2str(XLinear(3)/1e3,3) ' | Realistic aprox =  ' num2str(NLX(2*breakpoint)/1e3,3) ' (km) || z max: Linear aprox    = ' num2str(ZLinear(2)/1e3,4) '  | Realistic aprox =  ' num2str(NLZ(breakpoint)/1e3,4) ' (km)'];
+disp(str);
+str = ['x max: original values = ' num2str(NLX(2*breakpoint)/1e3,3) ' | T+100 values    =  ' num2str(NLX1(2*breakpoint1)/1e3,3) ' (km) || z max: original values = ' num2str(NLZ(breakpoint)/1e3,4) ' | T+100 values    =  ' num2str(NLZ1(breakpoint1)/1e3,4) '  (km)'];
+disp(str);
+str = ['x max: original values = ' num2str(NLX(2*breakpoint)/1e3,3) ' | 10*I0 values    =  ' num2str(NLX2(2*breakpoint2)/1e3,3) ' (km) || x max: original values = ' num2str(NLZ(breakpoint)/1e3,4) ' | 10*I0 values    =  ' num2str(NLZ2(breakpoint2)/1e3,4) ' (km)'];
+disp(str);
